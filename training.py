47bc42c0e04866ad71ccff70fa70a60e2a9b574b
leeseobin00 <leeseobin000709@gmail.com>
2023-12-08 02:47:29 +0900

    ✨[feat]: Initialize LBPH Face Recognizer and Cascade Classifier

--- training.py
+++ training.py
@@ -6,3 +6,5 @@ import face_recognition
 
 #Path for face image database
 path = '../opencv/opencv-4.1.2/data/haarcascades/haarcascade_frontalface_default.xml'
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier(path)
