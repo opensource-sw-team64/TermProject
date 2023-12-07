b85c1eba499d111735c11c3960fa6c4cf88ba465
leeseobin00 <leeseobin000709@gmail.com>
2023-12-08 03:58:56 +0900

    ✨[feat]: Print the End Message

--- training.py
+++ training.py
@@ -30,3 +30,6 @@ recognizer.train(faces, np.array(ids))
 
 # Save the model into trainer/trainer.yml
 recognizer.write('trainer/trainer.yml') # recognizer.save() worked on Mac, but not on Pi
+
+# Print the numer of faces trained and end program
+print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
