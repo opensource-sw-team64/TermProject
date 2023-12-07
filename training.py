908fbb2662c987b9ec7cf7d65be91f54bf8d3739
leeseobin00 <leeseobin000709@gmail.com>
2023-12-08 02:48:34 +0900

    ✨[feat]: Add function to retrieve image and label data

--- training.py
+++ training.py
@@ -8,3 +8,18 @@ import face_recognition
 path = '../opencv/opencv-4.1.2/data/haarcascades/haarcascade_frontalface_default.xml'
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 detector = cv2.CascadeClassifier(path)
+
+# function to get the images and label data
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+    faceSamples=[]
+    ids = []
+    for imagePath in imagePaths:
+        PIL_img = Image.open(imagePath).convert('L') # convert it to grayscale
+        img_numpy = np.array(PIL_img,'uint8')
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+    return faceSamples,ids
