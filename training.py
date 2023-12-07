67056a60aa9f8facb1d4159a0996e6d170789d47
leeseobin00 <leeseobin000709@gmail.com>
2023-12-08 02:47:05 +0900

    ✨[feat]: Set the path for the Haar Cascade classifier

--- training.py
+++ training.py
@@ -3,3 +3,6 @@ import numpy as np
 from PIL import Image
 import os
 import face_recognition
+
+#Path for face image database
+path = '../opencv/opencv-4.1.2/data/haarcascades/haarcascade_frontalface_default.xml'
