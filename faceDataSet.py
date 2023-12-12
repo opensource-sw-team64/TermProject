165c75e1da2b3219702700fdf3fe196f53571fc5
김동현 <oksean030330@gmail.com>
2023-12-12 22:06:16 +0900

    feat: Main Loop - Face Detection and Image Saving

--- faceDataSet.py
+++ faceDataSet.py
@@ -12,3 +12,20 @@ face_detector = cv2.CascadeClassifier(path)
 # Get user ID input and print initialization message
 face_id = input('\n enter user id end press <return> ==> ')
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+
+# Main loop for face detection and image saving
+count = 0
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h, x:x+w])
+        count += 1
+
+    cv2.imshow('image', img)
+    k = cv2.waitKey(100) & 0xff
+    if k == 27:
+        break
